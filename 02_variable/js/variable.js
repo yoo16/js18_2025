b090f219a1ef768f4224956054f374e76246830d
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2025-10-22 11:29:48 +0900

    add 02

---
+++ 02_variable/js/variable.js
@@ -0,0 +1,31 @@
+// TODO: 変数定義 商品名「itemName」
+
+// TODO: 変数定義 価格「price」
+
+// TODO: 変数定義 個数「quantity」
+
+// TODO: 変数定義 割引「discount」
+
+// TODO: itemName に再代入
+
+// TODO: 商品名をコンソール表示
+// clg
+
+// TODO: 価格, 個数, 割引
+
+// TODO: 商品名をHTML表示
+document.getElementById("item-name").innerHTML
+
+// TODO: 価格をHTML表示
+document.getElementById("price").innerHTML
+
+// TODO: 個数をHTML表示
+document.getElementById("quantity").innerHTML
+
+// TODO: 割引をHTML表示
+document.getElementById("discount").innerHTML
+
+// TODO: 定数定義 税率「TAX_RATE」
+
+// TODO: 税率をHTML表示
+document.getElementById("tax-rate").innerHTML
