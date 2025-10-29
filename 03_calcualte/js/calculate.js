cfe4684526c6a65a7e0aaef9eb9829b6a9c94152
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2025-10-29 12:12:05 +0900

    update

--- 03_calcualte/js/calculate.js
+++ 03_calcualte/js/calculate.js
@@ -39,8 +39,10 @@ discount = isMember ? 100 : 0
 totalPrice = subTotalPrice - discount
 
 // TODO: 税抜き金額 = 合計金額 / (1 + 税率): taxWithoutPrice
+taxWithoutPrice = totalPrice / (1 + TAX_RATE)
 
 // TODO: 税抜き金額を四捨五入: toFixed()
+taxWithoutPrice = taxWithoutPrice.toFixed(0)
 
 // TODO: 税額 = 合計金額 - 税抜き金額: tax
 
