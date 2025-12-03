6baf9371ac0b29649a1c87180db8a363fa6451c6
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2025-12-03 11:51:02 +0900

    update

--- 07_dom/js/dice.js
+++ 07_dom/js/dice.js
@@ -70,7 +70,9 @@ const rollDice = () => {
 
     const timer = setInterval(() => {
         number = randomNumber(1, 6);
-        // TODO: サイコロの目の表示更新
+        // TODO: サイコロの目の表示更新(update)
+        // diceContainer.innerHTML = number
+        diceContainer.innerHTML = `<span class="text-7xl">${number}</span>`
 
         // TODO: diceImages 配列から、number 番目のSVG画像を取り出して表示: innerHTML
 
